96556fa1fe03d18a190ef5faf7399729c15e1d00
Vedavyas Vundyala <vedvund@msitprogram.net>
2021-09-16 23:40:50 +0530

    test file added

---
+++ 1test.py
@@ -0,0 +1,12 @@
+first = True
+second = False
+
+
+if first == True:
+    if second == True:
+        print("both true!")
+    else:
+        print("only first is true")
+else:
+    print("first not true")
+
